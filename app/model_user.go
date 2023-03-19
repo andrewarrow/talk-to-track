9555fc9469f84c05e4774b420caeaadf045e7cf7
Andrew Arrow <oneone@gmail.com>
2023-03-19 11:51:07 -0700

    using generic update one

---
+++ app/model_user.go
@@ -0,0 +1,18 @@
+package app
+
+import "github.com/andrewarrow/feedback/router"
+
+func AfterCreateUser(c *router.Context, guid string) {
+	u := c.LookupUser(guid)
+
+	orgName := u.Username + "_org"
+	params := map[string]any{"name": orgName}
+	newGuid := c.Insert("org", params)
+
+	org := c.SelectOne("org", "where guid=$1", []any{newGuid})
+
+	c.UpdateOne("user", "org_id=$1", "user_id=$2", []any{org["id"], u.Id})
+
+	params = map[string]any{"org_id": org["id"], "name": orgName + "_project1"}
+	c.Insert("project", params)
+}
