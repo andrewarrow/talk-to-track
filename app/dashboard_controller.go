13000885202da3c04acbd513b2c3eba892ab6f8e
Andrew Arrow <oneone@gmail.com>
2023-03-19 13:16:57 -0700

    fixing for user change

--- app/dashboard_controller.go
+++ app/dashboard_controller.go
@@ -10,6 +10,6 @@ func HandleDashboard(c *router.Context, second, third string) {
 		return
 	}
 	params := []any{0}
-	rows := c.SelectAll("project", "where org_id=$1", params)
+	rows := c.SelectOne("org", "id=$1", params)
 	c.SendContentInLayout("dashboard.html", rows, 200)
 }
--- app/model_user.go
+++ app/model_user.go
@@ -5,13 +5,13 @@ import "github.com/andrewarrow/feedback/router"
 func AfterCreateUser(c *router.Context, guid string) {
 	u := c.LookupUser(guid)
 
-	orgName := u.Username + "_org"
+	orgName := u["username"].(string) + "_org"
 	params := map[string]any{"name": orgName}
 	newGuid := c.Insert("org", params)
 
 	org := c.SelectOne("org", "where guid=$1", []any{newGuid})
 
-	c.UpdateOne("user", "org_id=$1", "id=$2", []any{org["id"], u.Id})
+	c.UpdateOne("user", "org_id=$1", "id=$2", []any{org["id"], u["id"]})
 
 	params = map[string]any{"org_id": org["id"], "name": orgName + "_project1"}
 	c.Insert("project", params)
