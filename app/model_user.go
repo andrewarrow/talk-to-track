b6b06870ae26ccff45d93ca64a8e500819ad0046
Andrew Arrow <oneone@gmail.com>
2023-03-28 07:36:39 -0700

    using new funcs

--- app/model_user.go
+++ app/model_user.go
@@ -6,17 +6,22 @@ func AfterCreateUser(c *router.Context, guid string) {
 	u := c.LookupUser(guid)
 
 	orgName := u["username"].(string) + "_org"
-	params := map[string]any{"name": orgName}
-	newGuid := c.Insert("org", params)
+	c.Params = map[string]any{"name": orgName}
+	c.Validate("org")
+	c.Insert("org")
+	newGuid := c.Params["guid"]
 
 	org := c.SelectOne("org", "where guid=$1", []any{newGuid})
 
 	c.UpdateOne("user", "org_id=$1", "id=$2", []any{org["id"], u["id"]})
 
-	params = map[string]any{"org_id": org["id"], "name": orgName + "_project1"}
-	newGuid = c.Insert("project", params)
+	c.Params = map[string]any{"org_id": org["id"], "name": orgName + "_project1"}
+	c.Validate("project")
+	c.Insert("project")
+	newGuid = c.Params["guid"]
 	project := c.SelectOne("project", "where guid=$1", []any{newGuid})
 
-	params = map[string]any{"project_id": project["id"], "org_id": org["id"], "title": "sample issue one"}
-	c.Insert("issue", params)
+	c.Params = map[string]any{"project_id": project["id"], "org_id": org["id"], "title": "sample issue one"}
+	c.Validate("issue")
+	c.Insert("issue")
 }
